5b76736a22c313a7b56d4c61bfc8fc22e1c09152
Sanmathi87 <sanmathisanjay30@gmail.com>
2026-07-02 22:24:59 +0530

    Add files via upload

---
+++ task4.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>III CS B Timetable</title>
+</head>
+<body>
+
+<h2 align="center">III CS B - Timetable</h2>
+
+<table align="center" border="2" cellpadding="10" cellspacing="5">
+
+  <tr>
+    <th>D/O</th>
+    <th>1</th>
+    <th>2</th>
+    <th>3</th>
+    <th>4</th>
+    <th>5</th>
+  </tr>
+
+  <tr>
+    <td><b>I</b></td>
+    <td bgcolor="#d5f5e3">SE (MD)</td>
+    <td bgcolor="#d6eaf8">CG (PM)</td>
+    <td bgcolor="#fdebd0">VC / BD (SK/MR)</td>
+    <td bgcolor="#e8daef">WDD (KS)</td>
+    <td bgcolor="#e8daef">WDD (KS)</td>
+  </tr>
+
+  <tr>
+    <td><b>II</b></td>
+    <td bgcolor="#d5f5e3">SE (MD)</td>
+    <td bgcolor="#d6eaf8">CG (PM)</td>
+    <td bgcolor="#e8daef">WDD (KS)</td>
+    <td bgcolor="#d6eaf8">CG (PM)</td>
+    <td bgcolor="#d5f5e3">SE (MD)</td>
+  </tr>
+
+  <tr>
+    <td><b>III</b></td>
+    <td colspan="2" bgcolor="#f5b041">WDD Lab - Lab 11 (KS &amp; MR)</td>
+    <td bgcolor="#d6eaf8">CG (PM)</td>
+    <td bgcolor="#fdebd0">VC / BD (SK/MR)</td>
+    <td bgcolor="#f5cba7">SBS DevOps Theory (MD)</td>
+  </tr>
+
+  <tr>
+    <td><b>IV</b></td>
+    <td bgcolor="#e8daef">WDD (KS)</td>
+    <td bgcolor="#d5f5e3">SE (MD)</td>
+    <td bgcolor="#fdebd0">VC / BD (SK/MR)</td>
+    <td colspan="2" bgcolor="#f9e79f">CybSec (PM)</td>
+  </tr>
+
+  <tr>
+    <td><b>V</b></td>
+    <td colspan="2" bgcolor="#f5b041">WDD Lab - Lab 11 (KS &amp; SK)</td>
+    <td bgcolor="#f5b041">WDD Lab - Lab 11 (KS &amp; MR)</td>
+    <td bgcolor="#d6eaf8">CG (PM)</td>
+    <td bgcolor="#fdebd0">VC / BD (SK/MR)</td>
+  </tr>
+
+  <tr>
+    <td><b>VI</b></td>
+    <td bgcolor="#fdebd0">VC / BD (SK/MR)</td>
+    <td bgcolor="#e8daef">WDD (KS)</td>
+    <td bgcolor="#d5f5e3">SE (MD)</td>
+    <td colspan="2" bgcolor="#f5b041">SBS DevOps - Lab 11 (MD)</td>
+  </tr>
+
+</table>
+
+</body>
+</html>
