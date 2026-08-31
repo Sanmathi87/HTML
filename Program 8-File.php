9d056b8fa299efc79db667f7f341915c5c126d43
Sanmathi87 <sanmathisanjay30@gmail.com>
2026-08-31 13:07:42 +0530

    Update Program 8-File.php

--- Program 8-File.php
+++ Program 8-File.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Read and Write File</title>
+<title>Read and Write File</title>
 </head>
 <body>
 <?php
 $filename = "C:/xampp/htdocs/24sbcs087/sample1.txt";
 if (!file_exists($filename)) {
-    echo "File '$filename' does not exist";
-    exit();
+echo "File '$filename' does not exist";
+exit();
 }
 $file = fopen($filename, "r");
 if ($file == false) {
-    echo "Error in opening file";
-    exit();
+echo "Error in opening file";
+exit();
 }
 $filesize = filesize($filename);
 $filetext = fread($file, $filesize);
@@ -22,16 +22,16 @@ echo "File size: $filesize bytes<br>";
 echo "<pre>" . htmlspecialchars($filetext) . "</pre>";
 $file = fopen($filename, "w");
 if ($file == false) {
-    echo "Error in opening file for writing";
-    exit();
+echo "Error in opening file for writing";
+exit();
 }
 $text_to_write = "This is a simple text";
 fwrite($file, $text_to_write);
 fclose($file);
 $file = fopen($filename, "r");
 if ($file == false) {
-    echo "Error in opening file for reading";
-    exit();
+echo "Error in opening file for reading";
+exit();
 }
 $filesize = filesize($filename);
 $filetext = fread($file, $filesize);
